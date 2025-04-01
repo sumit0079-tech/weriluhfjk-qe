9be8b00468403a6b3fa0754f2ecb11c62735cc9e
sumit0079-tech <sumit.jurisnexa03@gmail.com>
2025-04-01 11:54:16 +0530

    gfhb

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-24 mt-24">
+        <h1 className="text-2xl bg-stone-500">Hello, Tailwind CSS!</h1>
+      </div>
+      <h1 >Welcome to Vite + React + Tailwind CSS</h1>
+      <div className="p-4">
+        <p className="text-gray-700">This is a simple example of using Tailwind CSS with Vite.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
